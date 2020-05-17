4b35c32ea8ceb99cf2423b4f1cf5e7b8e169b5d5
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-17 19:47:54 -0400

    working implementation with working memory database demo

--- src/android/SQLiteBatchConnectionManager.java
+++ src/android/SQLiteBatchConnectionManager.java
@@ -6,15 +6,64 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class SQLiteBatchConnectionManager extends CordovaPlugin {
   @Override
-  public boolean execute(String action, JSONArray args, CallbackContext cbc) {
-    switch (action) {
+  public boolean execute(String action, JSONArray data, CallbackContext cbc) {
+    switch(action) {
       case "openDatabaseConnection":
-        cbc.success(args);
-      return true;
+        openDatabaseConnection(data, cbc);
+        break;
+      case "executeBatch":
+        executeBatch(data, cbc);
+        break;
+      default:
+        return false;
     }
-    return false;
+    return true;
+  }
+
+  static private void
+  openDatabaseConnection(JSONArray args, CallbackContext cbc) {
+    try {
+      final JSONObject options = args.getJSONObject(0);
+
+      final String fullName = options.getString("fullName");
+
+      final int flags = options.getInt("flags");
+
+      SQLiteBatchCore.openBatchConnection(fullName, flags, new SQLiteBatchCore.OpenCallbacks() {
+        public void success(int connectionId) {
+          cbc.success(connectionId);
+        }
+        public void error(String message) {
+          cbc.error(message);
+        }
+      });
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void executeBatch(JSONArray args, CallbackContext cbc) {
+    try {
+      final int mydbc = args.getInt(0);
+
+      JSONArray data = args.getJSONArray(1);
+
+      SQLiteBatchCore.executeBatch(mydbc, data, new SQLiteBatchCore.ExecuteBatchCallbacks() {
+        public void success(JSONArray results) {
+          cbc.success(results);
+        }
+        public void error(String message) {
+          cbc.error(message);
+        }
+      });
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
   }
 }
--- src/ios/SQLiteBatchConnectionManager.m
+++ src/ios/SQLiteBatchConnectionManager.m
@@ -2,18 +2,80 @@
 
 #import <Cordova/CDVPlugin.h>
 
+#import "SQLiteBatchCore.h"
+
 @interface SQLiteBatchConnectionManager : CDVPlugin
 
-- (void) openDatabaseConnection : (CDVInvokedUrlCommand *) command;
+- (void) openDatabaseConnection: (CDVInvokedUrlCommand *) commandInfo;
+
+- (void) executeBatch: (CDVInvokedUrlCommand *) commandInfo;
 
 @end
 
 @implementation SQLiteBatchConnectionManager
 
-- (void) openDatabaseConnection : (CDVInvokedUrlCommand *) command
+- (void) openDatabaseConnection: (CDVInvokedUrlCommand *) commandInfo
 {
-  CDVPluginResult * result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:command.arguments];;
-  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+  NSArray * _args = commandInfo.arguments;
+
+  NSDictionary * options = (NSDictionary *)[_args objectAtIndex: 0];
+
+  NSString * filename = (NSString *)[options valueForKey: @"fullName"];
+
+  const int flags = [(NSNumber *)[options valueForKey: @"flags"] intValue];
+
+  [SQLiteBatchCore openBatchConnection: filename
+                                 flags: flags
+                               success: ^(int connection_id) {
+    CDVPluginResult * openResult =
+      [CDVPluginResult resultWithStatus: CDVCommandStatus_OK
+                           messageAsInt: connection_id];
+
+    [self.commandDelegate sendPluginResult: openResult
+                                callbackId: commandInfo.callbackId];
+  }
+  error: ^ (NSString * message) {
+    CDVPluginResult * openErrorResult =
+      [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR
+                        messageAsString: message];
+    [self.commandDelegate sendPluginResult: openErrorResult
+                                callbackId: commandInfo.callbackId];
+  }];
+}
+
+- (void) executeBatch: (CDVInvokedUrlCommand *) commandInfo
+{
+  dispatch_async(
+    dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+      [self executeBatchNow: commandInfo];
+    });
+}
+
+- (void) executeBatchNow: (CDVInvokedUrlCommand *) commandInfo
+{
+  NSArray * _args = commandInfo.arguments;
+
+  const int connection_id = [(NSNumber *)[_args objectAtIndex: 0] intValue];
+
+  NSArray * data = [_args objectAtIndex: 1];
+
+  [SQLiteBatchCore executeBatch: connection_id
+                           data: data
+                        success: ^(NSArray * results) {
+    CDVPluginResult * batchResult =
+      [CDVPluginResult resultWithStatus: CDVCommandStatus_OK
+                         messageAsArray: results];
+
+    [self.commandDelegate sendPluginResult: batchResult
+                                callbackId: commandInfo.callbackId];
+  }
+  error: ^ (NSString * message) {
+    CDVPluginResult * batchErrorResult =
+      [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR
+                        messageAsString: message];
+    [self.commandDelegate sendPluginResult: batchErrorResult
+                                callbackId: commandInfo.callbackId];
+  }];
 }
 
 @end
